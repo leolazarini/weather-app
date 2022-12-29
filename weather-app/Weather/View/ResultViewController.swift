8eaed201bd405684eefe3f2536e376433d1f2500
Leonardo Lazarini <leo.lazarini@gmail.com>
2022-12-29 16:12:03 -0300

    Views do projeto alteradas de Storyboard para XIB

--- weather-app/Weather/View/ResultViewController.swift
+++ weather-app/Weather/View/ResultViewController.swift
@@ -18,14 +18,14 @@ class ResultViewController: UIViewController {
     
     var weatherModel: WeatherModel!
     
-//    init(weatherModel: WeatherModel) {
-//        self.weatherModel = weatherModel
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+    init(weatherModel: WeatherModel) {
+        self.weatherModel = weatherModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
